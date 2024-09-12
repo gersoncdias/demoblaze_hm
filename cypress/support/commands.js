0c1a210dce686fcd2fb3d8d773e90c5da18f2ae5
Gerson Dias <gersoncdias1@gmail.com>
2024-09-12 09:27:46 -0300

    ajuste de duplicidade de impot

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,5 @@
 const HOME_PAGE = require('../support/page_elements/home_page').HOME_PAGE
 const HEADER_PAGE = require('../support/page_elements/header_page').HEADER_PAGE
-const HEADER_PAGE = require('../support/page_elements/header_page').HEADER_PAGE
 
 Cypress.Commands.add('validar_banner', () => {
     cy.get(HOME_PAGE.BODY.IMG_BANNER).should('be.visible')
