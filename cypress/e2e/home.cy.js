58271924cf04b5f86cd248c5f83d5e916635f9fe
Gerson Dias <gersoncdias1@gmail.com>
2024-09-16 10:17:06 -0300

    validação de footer

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,10 +1,20 @@
-const validation_dataheader = require('../fixtures/header.json')
+const validation_dataheader = require("../fixtures/header.json");
+const validation_data_footer = require("../fixtures/footer.json");
 
-describe('Validar Home Page', () => {
-  it('Deve acessar a pagina princípal', () => {
-    cy.visit('/')
-    cy.validar_banner()
-    cy.validar_logo()
-    cy.validar_links(validation_dataheader.menu.home, validation_dataheader.links.index )
-  })
-})
+describe("Validar Home Page", () => {
+  beforeEach(() => {
+    cy.visit("/");
+  });
+  it("Deve acessar a pagina princípal", () => {
+    cy.validar_banner();
+    cy.validar_logo();
+    cy.validar_links(
+      validation_dataheader.menu.home,
+      validation_dataheader.links.index
+    );
+  });
+  it.only("Deve validar o Footer", () => {
+    cy.validar_div();
+    cy.validateAboutUsSection(validation_data_footer.title, validation_data_footer.text)
+  });
+});
