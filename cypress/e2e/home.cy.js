7b5964577b9f0dd9f7b34b40a31c19eee22b45cd
Gerson Dias <gersoncdias1@gmail.com>
2024-10-04 09:47:49 -0300

    install faker

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,11 @@
 const validation_data_header = require("../fixtures/header.json");
 const validation_data_footer = require("../fixtures/footer.json");
+const { faker } = require('@faker-js/faker');
+
+const user = {
+  username: faker.internet.userName(),
+  password: faker.internet.password()
+};
 
 describe("Validar Home Page", () => {
   beforeEach(() => {
@@ -8,36 +14,49 @@ describe("Validar Home Page", () => {
   it("Deve acessar a pagina princípal", () => {
     cy.validar_banner();
     cy.validar_logo();
-
   });
-  it.only("Deve validar o Footer", () => {
+  it("Deve validar o Footer", () => {
     cy.validar_div();
-    cy.validateAboutUsSection(validation_data_footer.title, validation_data_footer.text)
-    cy.validateGetInTouchSection(validation_data_footer.get_touch, validation_data_footer.adress, validation_data_footer.telephone, validation_data_footer.email);
-    cy.validateProductStoreSection(validation_data_footer.product_store)
-    cy.validatecopyright(validation_data_footer.copyright)
+    cy.validateAboutUsSection(
+      validation_data_footer.title,
+      validation_data_footer.text
+    );
+    cy.validateGetInTouchSection(
+      validation_data_footer.get_touch,
+      validation_data_footer.adress,
+      validation_data_footer.telephone,
+      validation_data_footer.email
+    );
+    cy.validateProductStoreSection(validation_data_footer.product_store);
+    cy.validatecopyright(validation_data_footer.copyright);
   });
 
   it("Deve validar o header", () => {
-    cy.validateProductStore(validation_data_header.logo.text)
+    cy.validateProductStore(validation_data_header.logo.text);
     cy.validar_logo();
     cy.validar_links(
       validation_data_header.menu.home,
-      validation_data_header.links.index)
+      validation_data_header.links.index
+    );
     cy.validar_links(
-      validation_data_header.menu.contact, validation_data_header.links.deful)
+      validation_data_header.menu.contact,
+      validation_data_header.links.deful
+    );
     cy.validar_links(
-      validation_data_header.menu.about, validation_data_header.links.deful)
+      validation_data_header.menu.about,
+      validation_data_header.links.deful
+    );
     cy.validar_links(
       validation_data_header.menu.cart,
-      validation_data_header.links.cart)
+      validation_data_header.links.cart
+    );
     cy.validar_links(
-      validation_data_header.menu.login, validation_data_header.links.deful)
+      validation_data_header.menu.login,
+      validation_data_header.links.deful
+    );
     cy.validar_links(
-      validation_data_header.menu.sigin, validation_data_header.links.deful)
-
-          
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
   });
-  
 });
-
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -2,3 +2,4 @@ import "./commands/home_commands";
 import "cypress-plugin-xhr-toggle";
 import "./commands/header_commands";
 import "./commands/footer_commands";
+import 'cypress-plugin-api';
