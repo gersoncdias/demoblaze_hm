789d4aba1744c1836c7253f6946303be2c2165a4
mandy_silva <amanda.silva@qualitydigital.global>
2024-10-08 11:57:32 -0300

    signup_amanda

---
+++ cypress/support/page_elements/signup_page.js
@@ -0,0 +1,13 @@
+export const SINGUP_PAGE = {
+    MODAL:{
+        Titulo: '#signInModal',
+        username: '#sign-username',
+        senha: '#sign-password',
+        User: '.form-control-label',
+        password: '.form-control-label',
+        botão: '.btn-primary',
+        close: '#signInModal > .modal-dialog > .modal-content > .modal-footer > .btn-secondary'
+        
+    }
+
+}
