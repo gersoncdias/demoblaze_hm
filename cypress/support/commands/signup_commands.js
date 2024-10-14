bd53bc6ec92344385be4b24e3004784a154e10cd
mandy_silva <amanda.silva@qualitydigital.global>
2024-10-14 10:52:45 -0300

    signup finalizado

--- cypress/support/commands/signup_commands.js
+++ cypress/support/commands/signup_commands.js
@@ -9,42 +9,63 @@ Cypress.Commands.add("acessar_modal_signup", (txt, link) => {
       .should("have.attr", "href", link).click();
   });
 
-  Cypress.Commands.add('validar_modal_signup', (txt) => {
+Cypress.Commands.add('validar_modal_signup', (txt) => {
     cy.get( SINGUP_PAGE.MODAL.Titulo)
       .contains(txt) ;
   });
 
-  Cypress.Commands.add('validar_campos', (txt) => {
+Cypress.Commands.add('validar_campos', (txt) => {
     cy.get( SINGUP_PAGE.MODAL.Titulo)
       .contains(txt) ;
     });
 
-    Cypress.Commands.add('validar_inputs', ()=> {
+Cypress.Commands.add('validar_inputs_invalido', ()=> {
       cy.get( SINGUP_PAGE.MODAL.username).focus().clear().type('teste')
-      cy.get( SINGUP_PAGE.MODAL.senha).type('123')
+      cy.get( SINGUP_PAGE.MODAL.senha).type(' ')
       
      });
+Cypress.Commands.add('validar_inputs_valido', ()=> {
+      cy.get( SINGUP_PAGE.MODAL.username).focus().clear().type('teste')
+      cy.get( SINGUP_PAGE.MODAL.senha).type('123')
+     });
 
-     Cypress.Commands.add('validar_texto_user', (txt) => {
+
+Cypress.Commands.add('validar_texto_user', (txt) => {
       cy.get(SINGUP_PAGE.MODAL.User)
         .contains(txt) ;
     });
     
-    Cypress.Commands.add('validar_texto_senha', (txt) => {
+Cypress.Commands.add('validar_texto_senha', (txt) => {
       cy.get(SINGUP_PAGE.MODAL.password)
         .contains(txt) ;
     });
 
-    Cypress.Commands.add("clicar_no_botão_enviar", (txt) => {
+Cypress.Commands.add("clicar_no_botão_enviar", (txt) => {
       cy.get(SINGUP_PAGE.MODAL.botão)
         .should("be.visible")
         .contains(txt).click();
     });
 
-    Cypress.Commands.add("clicar_no_botão_cancelar", (txt) => {
+Cypress.Commands.add("clicar_no_botão_cancelar", (txt) => {
       cy.get(SINGUP_PAGE.MODAL.close)
         .should("be.visible")
         .should("have.text", txt).invoke('click');
       cy.get(SINGUP_PAGE.MODAL.close)
         .should("not.be.visible");
     });
+
+Cypress.Commands.add('validarAlert', (text) => {
+      cy.on('window:alert', (txt) => {
+        expect(txt).to.contains(text);
+      });
+});
+      
+
+Cypress.Commands.add('clicar_no_botao_X', () => {
+   cy.get(SINGUP_PAGE.MODAL.btn_header_close)
+          .should('be.visible')
+          .invoke('click');
+        cy.get(SINGUP_PAGE.MODAL.close)
+          .should("not.be.visible");
+});
+  
