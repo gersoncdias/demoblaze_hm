c01ea3c5311a13da5f28f0809f5845ca24d7483e
Gerson Dias <gersoncdias1@gmail.com>
2024-10-16 09:41:04 -0300

    ajuste de branch

--- cypress/e2e/singup.cy.js
+++ cypress/e2e/singup.cy.js
@@ -8,65 +8,73 @@ describe("Validar Home Page", () => {
 
   it("Deve acessar o modal", () => {
     cy.acessar_modal_signup(
-      validation_data_header.menu.sigin, validation_data_header.links.deful)
-          
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
   });
 
   it("valida nome do modal", () => {
-    cy.validar_modal_signup(validation_data_signup.modal.titulo)
-          
+    cy.validar_modal_signup(validation_data_signup.modal.titulo);
   });
-  
-  it("inserir dados",() => {
+
+  it("inserir dados", () => {
     cy.acessar_modal_signup(
-      validation_data_header.menu.sigin, validation_data_header.links.deful)
-    cy.validar_inputs()
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
   });
 
   it("valida nome do modal user", () => {
     cy.acessar_modal_signup(
-      validation_data_header.menu.sigin, validation_data_header.links.deful)
-    cy.validar_texto_user(validation_data_signup.modal.user)
-});
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
+    cy.validar_texto_user(validation_data_signup.modal.user);
+  });
 
-it("valida nome do modal senha", () => {
-  cy.acessar_modal_signup(
-    validation_data_header.menu.sigin, validation_data_header.links.deful)
-  cy.validar_texto_senha(validation_data_signup.modal.senha)
-});
+  it("valida nome do modal senha", () => {
+    cy.acessar_modal_signup(
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
+    cy.validar_texto_senha(validation_data_signup.modal.senha);
+  });
 
-it("Deve clicar no botão enviar", () => {
-  cy.acessar_modal_signup(
-    validation_data_header.menu.sigin, validation_data_header.links.deful)
-  cy.validar_inputs_valido()
-  cy.clicar_no_botão_enviar(
-    validation_data_signup.modal.botão)
-  cy.validarAlert('This user already exist.')
+  it("Deve clicar no botão enviar", () => {
+    cy.acessar_modal_signup(
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
+    cy.validar_inputs_valido();
+    cy.clicar_no_botão_enviar(validation_data_signup.modal.botão);
+    cy.validarAlert("This user already exist.");
+  });
 
-});
+  it("Deve clicar no botão enviar", () => {
+    cy.acessar_modal_signup(
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
+    cy.validar_inputs_invalido();
+    cy.clicar_no_botão_enviar(validation_data_signup.modal.botão);
+    cy.validarAlert("Please fill out Username and Password.");
+  });
 
-it("Deve clicar no botão enviar", () => {
-  cy.acessar_modal_signup(
-    validation_data_header.menu.sigin, validation_data_header.links.deful)
-  cy.validar_inputs_invalido()
-  cy.clicar_no_botão_enviar(
-    validation_data_signup.modal.botão)
-  cy.validarAlert('Please fill out Username and Password.')
-});
+  it("Deve clicar no botão cancelar", () => {
+    cy.acessar_modal_signup(
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
+    //cy.validar_inputs()
+    cy.clicar_no_botão_cancelar(validation_data_signup.modal.sair);
+  });
 
-it("Deve clicar no botão cancelar", () => {
-  cy.acessar_modal_signup(
-    validation_data_header.menu.sigin, validation_data_header.links.deful)
-  //cy.validar_inputs()
-  cy.clicar_no_botão_cancelar(
-    validation_data_signup.modal.sair)
+  it("Deve clicar no botão X", () => {
+    cy.acessar_modal_signup(
+      validation_data_header.menu.sigin,
+      validation_data_header.links.deful
+    );
+    //cy.validar_inputs()
+    cy.clicar_no_botao_X();
+  });
 });
-
-it.only("Deve clicar no botão X", () => {
-  cy.acessar_modal_signup(
-    validation_data_header.menu.sigin, validation_data_header.links.deful)
-  //cy.validar_inputs()
-  cy.clicar_no_botao_X()
-});
-});
-
