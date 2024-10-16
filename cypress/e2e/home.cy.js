e00ecb27e1c9832541875262af329ab7355c95dc
Gerson Dias <gersoncdias1@gmail.com>
2024-10-16 09:34:23 -0300

    remove .only

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -1,5 +1,5 @@
-const validation_data_header = require("../fixtures/header.json");
-const validation_data_footer = require("../fixtures/footer.json");
+const validation_data_header = require('../fixtures/header.json');
+const validation_data_footer = require('../fixtures/footer.json');
 const { faker } = require('@faker-js/faker');
 
 const user = {
@@ -7,11 +7,11 @@ const user = {
   password: faker.internet.password()
 };
 
-describe("Validar Home Page", () => {
+describe('Validar Home Page', () => {
   beforeEach(() => {
-    cy.visit("/");
+    cy.visit('/');
   });
-  it("Deve acessar a pagina princípal", () => {
+  it('Deve acessar a pagina princípal', () => {
     cy.validar_banner();
     cy.validar_logo();
     cy.validar_links(
@@ -19,12 +19,19 @@ describe("Validar Home Page", () => {
       validation_dataheader.links.index
     );
   });
-  it.only("Deve validar o Footer", () => {
-  it.only("Deve validar o Footer", () => {
+  it('Deve validar o Footer', () => {
     cy.validar_div();
-    cy.validateAboutUsSection(validation_data_footer.title, validation_data_footer.text)
-    cy.validateGetInTouchSection(validation_data_footer.get_touch, validation_data_footer.adress, validation_data_footer.telephone, validation_data_footer.email);
-    cy.validateProductStoreSection(validation_data_footer.product_store)
-    cy.validatecopyright(validation_data_footer.copyright)
+    cy.validateAboutUsSection(
+      validation_data_footer.title,
+      validation_data_footer.text
+    );
+    cy.validateGetInTouchSection(
+      validation_data_footer.get_touch,
+      validation_data_footer.adress,
+      validation_data_footer.telephone,
+      validation_data_footer.email
+    );
+    cy.validateProductStoreSection(validation_data_footer.product_store);
+    cy.validatecopyright(validation_data_footer.copyright);
   });
 });
